d63e1d348e01e284995a2c3c50efbd8a44d4d838
Ratnawat, Himanshu | Ratna | RMI <himanshu.ratnawat@rakuten.com>
2023-01-06 16:46:21 +0900

    emotions

--- src/Data/data.js
+++ src/Data/data.js
@@ -9,7 +9,7 @@ const moodData = {
         "Gratitude": "Gratitude is a powerful catalyst for happiness. It's the spark that lights a fire of joy in your soul",
         "Amusement": "Keep yourself amused and others confused.",
         "Playfulness": "The very essence of playfulness is an openness to anything that may happen, the feeling that whatever happens, it's okay... you're either free to play, or you're not.",
-        "custom": "custom"
+        "custom": "Be happy. Be bright. Be you."
     },
     "Sad": {
         "Gloomy": "No matter how gloomy the morning,it is a new day where anything is possible.",
@@ -18,7 +18,7 @@ const moodData = {
         "Unhappy": "There you go...let it all slide out. Unhappiness can't stick in a person's soul when it's slick with tears.",
         "Lonely ": "Sometimes you have to stand alone just to make sure you still can.",
         "Bored": "Is life not a thousand times too short for us to bore ourselves? Is life not a thousand times too short for us to bore ourselves?",
-        "custom": "custom"
+        "custom": "You're not alone my dear friend. Just get up and look around, there are many reasons to be happy."
     },
 
     "Fear": {
@@ -27,7 +27,7 @@ const moodData = {
         "Apprehensive": "No Matter how hard it gets, stick your chest out, keep your head up, and handle it.",
         "Anxious": "Anxiety is the dizziness of the freedom.", "Panic ": "Breathe in and out as slowly, deeply and gently as you can. Close your eyes genly and focus on your breathing.",
         "Insecure": "The same boiling water that softens the potato hardens the egg. It’s what you’re made of. Not the circumstances.", "Discouraged": "Do not get discouraged nor be in despair, the Divine Being, God is with you, he is your present helper in times of trouble.",
-        "custom": "custom"
+        "custom": "The brave man is not he who does not feel afraid, but he who conquers that fear."
     },
     "Anger": {
         "Irritated": "Holding on to anger is like grasping a hot coal with the intent of throwing it at someone else; you are the one who gets burned.",
@@ -35,7 +35,7 @@ const moodData = {
         "Annoyed": "Get some exercise, take a timeout and once you are calm express your concern",
         "Skeptical": "Pinpoint your insecurities, and try to notice the ones that are strongest * Identify your accomplishments",
         "Jealous": "Don't waste time on jealousy. Sometimes you're ahead, sometimes you're behind.",
-        "custom": "custom"
+        "custom": "For every minute you remain angry, you give up sixty seconds of peace of mind."
     },
     "Disgust": {
         "Repulsed": "It's ok to walk out of something if you feel you do not belongs to it.",
@@ -43,7 +43,7 @@ const moodData = {
         "Distaste": "A distaste for the new is not always fear of the unknown, but sometimes ambition. Some people don't like the new way simply because they never got a chance to master the old way.",
         "Repelled": "We are disgusted by the things that we desire, and we desire what disgusts us.",
         "Sickened ": "Sometimes we're tested not to show our weakness, but to discover our strengths.",
-        "custom": "custom"
+        "custom": "It's ok to walk out of something if you feel you do not belongs to it."
     },
     "Surprise": {
         "Shocked": "Those who are easily shocked should be shocked more often.",
@@ -51,7 +51,7 @@ const moodData = {
         "Amazed": "Life is a fairy tale. Live it with wonder and amazement.",
         "Stunned": "Great ! Everything has beauty, but not everyone sees it.",
         "Wonderment": "Never stop wandering into wonder.",
-        "custom": "custom"
+        "custom": "Surprise is the greatest gift which life can grant us"
     }
 };
 
